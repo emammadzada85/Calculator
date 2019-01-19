a133d59fc0cd139d75a0efdb3528130b33054c0a
Elshad R Mammadzada <45316551+emammadzada85@users.noreply.github.com>
2019-01-19 13:03:35 -0500

    numbers changed on code
    try pull changed code to your local repo

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,7 +2,7 @@ public class Calculator {
 
     public static void main(String[] args) {
 
-        int num = calcultor(100, 50, '+');
+        int num = calcultor(500, 250, '+');
         System.out.println(num);
     }
 
