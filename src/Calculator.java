7e12c4d8aa020b38670560ed5a05bb6c9c1978f3
Elshad Mammadzada <m.elshad@gmai.com>
2019-01-19 12:22:36 -0500

    addint more

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,11 +1,31 @@
 public class Calculator {
 
-    public static double add(double num1, double num2){
-        return num1+num2;
+    public static void main(String[] args) {
 
+        int num = calcultor(100, 50, '/');
+        System.out.println(num);
     }
-    public void minus(){
 
+    public static int calcultor(int x, int y, char operator){
+
+        int result = 0;
+
+        switch (operator){
+            case '+':
+                result = x + y;
+                return result;
+
+            case '-':
+                result = x - y;
+                return result;
+            case '*':
+                result =  x * y;
+                return result;
+            case '/':
+                result = x / y;
+                return result;
+        }
+        return result;
     }
 
 }
