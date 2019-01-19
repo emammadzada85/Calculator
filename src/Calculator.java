86e1ce9b90f44e5df2bd98dac3c72e3c6dae91ee
Elshad R Mammadzada <45316551+emammadzada85@users.noreply.github.com>
2019-01-19 13:59:00 -0500

    line changed

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,6 +3,7 @@ public class Calculator {
     public static void main(String[] args) {
         
         Scanner scan = new Scanner (System.in); 
+        System.out.println("gbgbgyby");
         
         int num = scan.nextInt(); 
 
